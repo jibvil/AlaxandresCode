73b85c8c8700bbd7c8d78a90862282fd3d13c8bd
jibvil <alaxandrechalifour@gmail.com>
2024-09-17 15:19:38 -0600

    9/17/2024

--- Interactive Scene Assignment/sketch.js
+++ Interactive Scene Assignment/sketch.js
@@ -5,11 +5,22 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+// makes the fly follow the cursor
+let flyX = mouseX;   let flyY = mouseY;
 
 function setup() {
   createCanvas(500, 500);
+  
 }
 
 function draw() {
-  background("Cyan");
+  background("Green");
+  // Making the fly
+  fill("Black");
+  rect(mouseX, mouseY, 10, 5, 90, 90);
+  // Background crap
+  
+  fill("Gray");
+  rect(300, 250, 150, 200,);
+
 }
