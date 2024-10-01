f769182cd00ffabcdacdcd99a02336e0190f6683
jibvil <alaxandrechalifour@gmail.com>
2024-10-01 15:19:26 -0600

    10/1/2024
    basically got nothing done

---
+++ Perlin Noise Assignment/sketch.js
@@ -0,0 +1,19 @@
+// Perlin Noise Project
+// Your Name
+// Date
+
+let noiseVar = 10;
+let rectHeight = 10;
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
+
+function generateTerrain() {
+  code
+}
