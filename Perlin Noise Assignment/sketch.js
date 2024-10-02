c1c56db6b1bee27f908ad1b4ed00ebbde13bd047
jibvil <alaxandrechalifour@gmail.com>
2024-10-02 15:19:50 -0600

    10/2/2024
    did like one thing today and that's it

--- Perlin Noise Assignment/sketch.js
+++ Perlin Noise Assignment/sketch.js
@@ -1,10 +1,8 @@
 // Perlin Noise Project
 // Your Name
 // Date
-
 let noiseVar = 10;
-let rectHeight = 10;
-
+let rectHeight = 1
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -12,8 +10,17 @@ function setup() {
 
 function draw() {
   background(220);
+  generateTerrain();
 }
 
 function generateTerrain() {
-  code
+    //draw a staircase using loops
+    randomSeed(2);
+    for(let x = 0; x < width; x += 20){
+      //20...rectangle spacing / rectWidth
+      fill(0);
+      noStroke();
+      let rectHeight = random(50,500);
+      rect(x, height, 20, -rectHeight);
+    }
 }
