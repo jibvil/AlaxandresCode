482c088fe78436f2a165e40f8b9b77397068659e
jibvil <alaxandrechalifour@gmail.com>
2024-10-03 15:17:40 -0600

    10/3/2024
    half asleep and kyle did most of the work

--- Perlin Noise Assignment/sketch.js
+++ Perlin Noise Assignment/sketch.js
@@ -2,7 +2,8 @@
 // Your Name
 // Date
 let noiseVar = 10;
-let rectHeight = 1
+let rectWidth = 5;
+let rectHeight;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -16,11 +17,23 @@ function draw() {
 function generateTerrain() {
     //draw a staircase using loops
     randomSeed(2);
-    for(let x = 0; x < width; x += 20){
+    for(let x = 0; x < width; x += rectWidth){
       //20...rectangle spacing / rectWidth
-      fill(0);
-      noStroke();
-      let rectHeight = random(50,500);
-      rect(x, height, 20, -rectHeight);
+      fill(200,200,200);
+      rectHeight = noise(noiseVar);
+      rectHeight = map(rectHeight, 0, 1, 0, height);
+      rect(x, height, rectWidth, -rectHeight);
     }
+
+    noiseVar += 0.01;
+}
+
+function keyPressed() {
+  if(keyCode === LEFT_ARROW && rectWidth > 1){
+    rectWidth -= 1;
+
+  }
+  if(keyCode === RIGHT_ARROW){
+    rectWidth += 1;
+  }
 }
