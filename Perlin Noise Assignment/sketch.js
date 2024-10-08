177dbdc0c022cba52e5354c2cade0d435dd6c3db
jibvil <alaxandrechalifour@gmail.com>
2024-10-08 15:24:00 -0600

    10/8/2024
    finished base features for terrain assignment

--- Perlin Noise Assignment/sketch.js
+++ Perlin Noise Assignment/sketch.js
@@ -1,7 +1,7 @@
 // Perlin Noise Project
 // Your Name
 // Date
-let noiseVar = 10;
+let noiseVar = 0;
 let rectWidth = 5;
 let rectHeight;
 
@@ -11,21 +11,24 @@ function setup() {
 
 function draw() {
   background(220);
+  resetNoiseVars();
   generateTerrain();
 }
 
 function generateTerrain() {
+
+  let localNoiseVar = noiseVar;
+
     //draw a staircase using loops
     randomSeed(2);
     for(let x = 0; x < width; x += rectWidth){
       //20...rectangle spacing / rectWidth
       fill(200,200,200);
-      rectHeight = noise(noiseVar);
+      rectHeight = noise(localNoiseVar);
       rectHeight = map(rectHeight, 0, 1, 0, height);
       rect(x, height, rectWidth, -rectHeight);
+      localNoiseVar += 0.01;
     }
-
-    noiseVar += 0.01;
 }
 
 function keyPressed() {
@@ -36,4 +39,9 @@ function keyPressed() {
   if(keyCode === RIGHT_ARROW){
     rectWidth += 1;
   }
+}
+
+function resetNoiseVars() {
+  //sets a bunch of variables back
+  noiseVar += 0.01;
 }
