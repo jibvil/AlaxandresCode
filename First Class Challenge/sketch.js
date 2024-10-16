d1cb196ebf56a8c2fe6aab71667cf898aaa7d28a
jibvil <alaxandrechalifour@gmail.com>
2024-10-16 15:21:08 -0600

    10/16/2024
    did color stuff and made the screen look like rainbow vomit

--- First Class Challenge/sketch.js
+++ First Class Challenge/sketch.js
@@ -2,51 +2,85 @@
 // Your Name
 // Date
 //
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+
 
 let points = [];
+const SEGMENT_REACH = 250;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(255);
-  for(let i = 0; i < points.length;i++){
+  background(0);
+  for (let i = 0; i < points.length; i++) {
     points[i].move();
+    points[i].connectPoints(points);
     points[i].display();
   }
 }
 
-function mouseClicked(){
+function mouseClicked() {
   points.push(new MiniPoint(mouseX, mouseY));
 }
 
-class MiniPoint{
-  constructor(x, y){
+class MiniPoint {
+  constructor(x, y) {
     this.x = x;
     this.y = y;
     this.c = color(random(255), random(255), random(255));
     this.s = 20;
     this.noiseX = random(10);
     this.noiseY = random(10);
-    this.offset = 0.1;
+    this.OFFSET = 0.1;
     this.MAX_SPEED = 5;
   }
 
-  display(){
+  display() {
     fill(this.c);
     noStroke();
-    ellipse(this.x, this.y, this.c, this.s);
+    ellipse(this.x, this.y, this.s, this.s);
   }
 
-  move(){
-    let xSpeed = map(noise(this.noiseX),0,1,-this.MAX_SPEED,this.MAX_SPEED);
-    let ySpeed = map(noise(this.noiseY),0,1,-this.MAX_SPEED,this.MAX_SPEED);
+  connectPoints(pointArray) {
+    stroke(this.c);
+    for (let i = 0; i < pointArray.length; i++) {
+      if (this !== pointArray[i]) {
+        if (dist(this.x, this.y, pointArray[i].getX(), pointArray[i].getY()) < SEGMENT_REACH) {
+          line(this.x, this.y, pointArray[i].getX(), pointArray[i].getY());
+        }
+      }
+    }
+  }
+
+  getX() {
+    return this.x;
+  }
+  getY() {
+    return this.y;
+  }
+
+  move() {
+    let xSpeed = map(noise(this.noiseX), 0, 1, -this.MAX_SPEED, this.MAX_SPEED);
+    let ySpeed = map(noise(this.noiseY), 0, 1, -this.MAX_SPEED, this.MAX_SPEED);
     this.x += xSpeed;
     this.y += ySpeed;
-    this.noiseX += this.offset;
-    this.noiseY += this.offset;
+    this.noiseX += this.OFFSET;
+    this.noiseY += this.OFFSET;
+
+    //
+    if (this.x < 0) {
+      this.x += width;
+    }
+    if (this.x > width) {
+      this.x -= width;
+    }
+    if (this.y < 0) {
+      this.y += height;
+    }
+    if (this.y > height) {
+      this.y -= width;
+    }
+
   }
 }
