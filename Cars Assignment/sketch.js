b17e99e3ab63d00c131ba5e399ab0e2f1590a51a
jibvil <alaxandrechalifour@gmail.com>
2024-10-21 15:19:37 -0600

    10/21/2024
    made epic road and grass now

--- Cars Assignment/sketch.js
+++ Cars Assignment/sketch.js
@@ -11,9 +11,25 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  background("Green");
+  drawRoad();
 }
 
 function drawRoad(){
-  
+  //gotta have a place for cars to drive on y'know!
+
+  noStroke();
+  fill(30);
+  rect(0, windowHeight/3, windowWidth, windowHeight/3);
+
+  //lines for the cars to stay between
+
+  strokeWeight(4);
+  stroke(255, 255, 255);
+  line(0, windowHeight/3 + 20, windowWidth, windowHeight/3 + 20);
+  line(0, windowHeight/3 * 2 - 20, windowWidth, windowHeight/3 * 2 - 20);
+  stroke("Yellow");
+  drawingContext.setLineDash([20, 20]);
+  line(0, windowHeight/2, windowWidth, windowHeight/2);
 }
+
