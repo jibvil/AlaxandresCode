f3b9a605ddf7f86e87923c518abe4fd7c3f74cad
jibvil <alaxandrechalifour@gmail.com>
2024-11-14 15:20:42 -0600

    11/14/2024
    did most of the work for balloon tree assignment

--- Balloon Tree Assignment/sketch.js
+++ Balloon Tree Assignment/sketch.js
@@ -3,7 +3,7 @@
 // 11/13/2024
 
 let scale = 15;
-
+let seed;
 
 function setup() {
   createCanvas(500, 500);
@@ -17,18 +17,34 @@ function draw() {
 
 function drawLine( x1,  y1,  x2,  y2,  depth) {
   //draw a line segment connecting (x1,y1) to (x2,y2)
+  strokeWeight(depth-1);
   line(x1, y1, x2, y2);
 }
 
+function drawLeaf(x2,y2,scale){
+  //add a leaf to the end of each line
+  strokeWeight(1);
+  fill(random(255),random(255),random(255));
+  circle(x2, y2, scale);
+}
 function drawTree(x1, y1, angle, depth) {
   if (depth > 0) {
     let x2 = x1 + (cos(radians(angle))*depth*scale);     //calculate endpoints of current branch
     let y2 = y1 - (sin(radians(angle))*depth*scale);     //using trig ratios. Get shorter based on depth
 
+
     drawLine(x1, y1, x2, y2, depth);
 
-    //for a 2-branch tree:
+    //for a 3-branch tree:
     drawTree(x2, y2, angle-18, depth-1);
     drawTree(x2, y2, angle+18, depth-1);
+    drawTree(x2, y2, angle+0, depth-1);
+
+    //for leaves to only spawn on a depth less than 5:
+    if (depth < 5){
+      drawLeaf(x2, y2, random(scale*3-depth*5));
+    } 
+
+    
   }
 }
